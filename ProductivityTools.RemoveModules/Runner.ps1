2e2ed541547114466566acb0651e99757eb7b1c0
Pawel Wujczyk <pwujczyk@gmail.com>
2025-01-23 21:03:01 +0100

    run as admin

--- ProductivityTools.RemoveModules/Runner.ps1
+++ ProductivityTools.RemoveModules/Runner.ps1
@@ -2,4 +2,5 @@ Clear-Host
 Set-Location $PSScriptRoot
 
 Import-Module .\ProductivityTools.RemoveModules.psm1 -Force 
-Remove-Modules -Name ProductivityTools. -StartsWith -Verbose
+Remove-ProductivityToolsModules
+#Remove-Modules -Name ProductivityTools. -StartsWith -Verbose
